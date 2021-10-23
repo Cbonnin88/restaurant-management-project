36b18887acd9b3c52d84d1ac713bc4a825699892
Chris <chrisbit.carrington@gmail.com>
2021-10-23 18:50:37 +0200

    Added Http Requests to foodRouter and userRouter

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,17 @@
 package routes
+
+
+import (
+	controller "RestaurantManagement/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserRoutes(incomingroutes *fiber.App) error {
+	incomingroutes.Get("/users", controller.GetUsers())
+	incomingroutes.Get("/users/:user_id",controller.GetUser())
+	incomingroutes.Post("/users/signup", controller.SignUp())
+	incomingroutes.Post("/users/login", controller.Login())
+	return nil
+}
+
+
