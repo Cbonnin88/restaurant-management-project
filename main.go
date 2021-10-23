23b00ccac96a436c234a21e1a6e5dc43e3c2c400
Chris <chrisbit.carrington@gmail.com>
2021-10-23 18:26:52 +0200

    Added elements to our main.go file

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	_ "RestaurantManagement/database"
-	_ "RestaurantManagement/middleware"
-	_ "RestaurantManagement/routes"
+
+
+	"RestaurantManagement/middleware"
+	"RestaurantManagement/routes"
+	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"os"
 )
+
+var foodCollection, err = sql.Open("mysql", "root:pwd@tcp("+
+	"localhost:3306)/restaurant_management")
 
 func main() {
 	port := os.Getenv("Port")
@@ -19,4 +25,23 @@ func main() {
 
 	router := fiber.New()
 	router.Use(logger.New())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItems(router)
+	routes.InvoiceRoutes(router)
+
+
+	err := router.Listen(":" + port)
+	if err != nil {
+		fmt.Print("Error: Unable to show page")
+		return
+	}
+
+
+
 }
